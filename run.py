a009d34a9260a64a9e057f8db90bea9b0843f28d
Cam-4587 Conroy <cameronconroy@live.com>
2024-12-18 12:50:15 +0000

    Finish Project

--- run.py
+++ run.py
@@ -1,3 +1,32 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+while True:
+    try:
+        num1 = int(input("What is your first Number: "))
+        if num1 < 0:
+            print("You need to select a positive number")
+            continue
+    except ValueError:
+        print("The value you entered isn't a number, please enter a valid number.")
+        continue
+    
+    try:
+        num2 = int(input("What is your second Number: "))
+        if num2 < 0:
+            print("You need to select a positive number")
+            continue
+    except ValueError:
+        print("The value you entered isn't a number, please enter a valid number.")
+        continue
+
+    nums = range(num1,num2)
+
+    def is_prime(num):
+        for x in range(2,num):
+            if (num%x) == 0:
+                return False
+            else:
+                return True
+
+    primes=list(filter(is_prime, nums))
+    print(primes)
+
+    break
